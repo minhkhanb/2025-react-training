76ed3662cbf41ed2e9fb7a5e70c54495b7c30f05
Qizhy <dashlyuidesigns@gmail.com>
2025-05-13 15:48:04 +0700

    Implement Custom Form Provider (#6)

--- src/components/ui/Form/index.tsx
+++ src/components/ui/Form/index.tsx
@@ -1,96 +1,49 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import React, { useId, useRef } from 'react';
-import { withProperties } from '@src/utils/types';
-import { FormProvider, FormState, useForm } from 'react-hook-form';
-import * as yup from 'yup';
+import React, { FormEvent, ReactNode, useId, useRef } from 'react';
+import { useForm, FormProvider, UseFormReturn, DefaultValues } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-
-enum SubmitMode {
-  onSubmitButton = 'onSubmitButton',
-  onChangeDebounced = 'onChangeDebounced',
-}
-
-export interface FormProps {
+import * as yup from 'yup';
+interface Props {
   className?: string;
-  children: any;
-  submitMode?: SubmitMode; // Specify when onSubmit is called.
-  onSubmit?: (
-    values: any,
-    defaultValues?: any,
-    formState?: FormState<any>,
-    formHandlers?: any
-  ) => any;
-  validationSchema?: yup.Lazy<any> | yup.ObjectSchema<any>;
-  defaultValues?: any;
-  mode?: 'onBlur' | 'onSubmit' | 'onChange';
-  showUnsavedChangesDialog?: boolean;
+  children: ReactNode;
+  validationSchema: yup.ObjectSchema<any>; // Yup schema
+  defaultValues: any;
+  onSubmit: (data: any, methods: UseFormReturn<any>, defaultValues: DefaultValues<any>) => void;
+  mode?: 'onBlur' | 'onChange' | 'onSubmit';
 }
 
-interface FormOptions {
-  submitMode?: SubmitMode;
-  onSubmit?: (
-    values: any,
-    defaultValues?: any,
-    formState?: FormState<any>,
-    formHandlers?: any
-  ) => any;
-  formId: string;
-}
-
-const FormOptionsContext = React.createContext<FormOptions>({} as FormOptions);
-
-const Form = ({
-  mode,
-  validationSchema,
-  submitMode = SubmitMode.onSubmitButton,
-  defaultValues = {},
-  showUnsavedChangesDialog: _,
+export function Form({
   children,
-  ...props
-}: FormProps) => {
-  const formHandlers = useForm({
-    mode,
+  className,
+  validationSchema,
+  defaultValues,
+  onSubmit,
+  mode = 'onBlur',
+}: Props) {
+  const ref = useRef<HTMLFormElement | null>(null);
+
+  const id = useId();
+
+  const methods = useForm({
     resolver: validationSchema && yupResolver(validationSchema),
     defaultValues,
+    mode,
   });
 
-  const formRef = useRef<HTMLFormElement>(null);
-  const [formId] = useId();
-
-  const onSubmit = async (evt: React.FormEvent<HTMLFormElement>) => {
-    if (evt) {
-      if (typeof evt.preventDefault === 'function') {
-        evt.preventDefault();
-      }
-
-      if (typeof evt.stopPropagation === 'function') {
-        // Prevent any outer forms from receiving the submit event
-        evt.stopPropagation();
-      }
-    }
-
-    return formHandlers.handleSubmit(async values => {
-      try {
-        if (props.onSubmit) {
-          await props.onSubmit(values, defaultValues, formHandlers.formState, formHandlers);
-        }
-      } catch (err) {
-        console.log('PDebug err debug: ', err);
-      }
-    })(evt);
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    return methods.handleSubmit(values => {
+      onSubmit(values, methods, defaultValues);
+    })(e);
   };
 
   return (
-    <FormOptionsContext.Provider value={{ submitMode, onSubmit, formId }}>
-      <FormProvider {...formHandlers}>
-        <form {...props} id={formId} ref={formRef} onSubmit={onSubmit}>
-          {children}
-          <input className="border" {...formHandlers.register('username')} />
-        </form>
-      </FormProvider>
-    </FormOptionsContext.Provider>
+    <FormProvider {...methods}>
+      <form id={id} ref={ref} className={className} onSubmit={handleSubmit}>
+        {children}
+      </form>
+    </FormProvider>
   );
-};
+}
 
-export default withProperties(Form, {});
+export default Form;
--- src/validattions/TaskSchema.ts
+++ src/validattions/TaskSchema.ts
@@ -1,8 +1,8 @@
 import * as yup from 'yup';
 
-export const taskSchema = yup.object({
-  title: yup.string().required('Task name is required'),
-  subtitle: yup.string().required('Task description is required'),
+export const taskSchema = yup.object().shape({
+  title: yup.string().required('Title is required'),
+  subtitle: yup.string().required('Subtitle is required'),
 });
 
 export type FormValues = yup.InferType<typeof taskSchema>;

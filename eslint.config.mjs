9ef2bf0ed639cedccd95825abd2477ef643beec0
Bao Duong <dhbao2505@gmail.com>
2025-05-19 16:58:04 +0700

    chore: update ESLint and Prettier configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,29 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigNext from 'eslint-config-next';
+import prettierConfig from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+import js from '@eslint/js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  js.configs.recommended,
+  eslintConfigNext,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: eslintPluginPrettier
+    },
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-console': 'warn',
+      'no-unused-vars': 'error'
+    }
+  }
 ];
-
-export default eslintConfig;

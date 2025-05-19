6fdb19b882c363247b22325fdc82d155eb090ac9
Kha Le <khale.fullstack@gmail.com>
2025-05-19 16:37:21 +0700

    init shadcn-ui

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  productionBrowserSourceMaps: process.env.NODE_ENV === 'development',
 };
 
 export default nextConfig;

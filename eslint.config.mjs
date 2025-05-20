1cdf93e78c70379ff9b2bad55b80d85d95f8343f
Duong Hoai Bao <dhbao2505@gmail.com>
2025-05-20 11:06:48 +0700

    Feature/setup environment (#22)
    - init nextjs@15.3.2 project
    - config husky and lint-staged
    - config prettierrc and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettier from 'eslint-plugin-prettier';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    plugins: {
+      prettier: prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'arrow-body-style': 'off',
+      'prefer-arrow-callback': 'off',
+      'react/display-name': 'off',
+    },
+  },
+];
+
+export default eslintConfig;

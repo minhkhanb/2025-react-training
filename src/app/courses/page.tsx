44c5ba0e76903e664fad7a1b22b6cc969df50284
Kha Le <khale.fullstack@gmail.com>
2025-05-21 18:12:34 +0700

    feat: react-table with pagination

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,8 +1,5 @@
 import Courses from '@src/section/Courses';
-import { makeData } from '@src/section/Courses/makeData';
 
 export default function CoursesPage() {
-  const initialData = makeData(1_000);
-
-  return <Courses initialData={initialData} />;
+  return <Courses />;
 }

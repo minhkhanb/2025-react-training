a36f92999c2ad1fa5db6b49cd3b73f068a985ac8
TRƯƠNG LÊ MINH NHẬT <147235072+nhattlm3173@users.noreply.github.com>
2025-05-26 09:02:23 +0700

    Feature/react table (#45)

--- src/section/Todo/components/Summary/index.tsx
+++ src/section/Todo/components/Summary/index.tsx
@@ -1,18 +1,17 @@
 'use client';
 
-import { useMemo } from 'react';
-import { TodoValue } from '../../types/ITodoList';
-
-export const Summary = ({ todos }: { todos: TodoValue[] }) => {
-  const todoCompleted = useMemo(() => {
-    return todos.filter(item => item.isFinish);
-  }, [todos]);
-
+export const Summary = ({
+  totalTodos,
+  todosCompleted,
+}: {
+  totalTodos: number;
+  todosCompleted: number;
+}) => {
   return (
     <div className="flex justify-between bg-gray-50 p-4 text-sm text-gray-600">
-      <span>Total: {todos.length} tasks</span>
+      <span>Total: {totalTodos} tasks</span>
 
-      <span>Completed: {todoCompleted.length}</span>
+      <span>Completed: {todosCompleted}</span>
     </div>
   );
 };

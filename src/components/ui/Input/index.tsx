d2e951d3d41f548a6ac62ff66124d252f397462e
KhaLeᴮᵉᵗᵗᵉʳ ᴱᵛᵉʳʸᵈᵃʸ <minhkhanb@gmail.com>
2025-05-28 18:07:43 +0700

    chore: dropdown ui (#68)
    Co-authored-by: Kha Le <khale.fullstack@gmail.com>

--- src/components/ui/Input/index.tsx
+++ src/components/ui/Input/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { withProperties } from '@src/utils/types';
+import { Input as InputCn } from '@src/components/shadcn/ui/input';
+import TextArea from '@src/components/ui/Input/TextArea';
+import Dropdown from '@src/components/ui/Input/Dropdown';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   inputRef?: React.Ref<HTMLInputElement>;
@@ -9,7 +12,7 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 const Input = ({ inputRef, value, ...props }: InputProps) => {
   return (
     <div>
-      <input
+      <InputCn
         type="text"
         autoComplete="given-name"
         className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
@@ -22,4 +25,4 @@ const Input = ({ inputRef, value, ...props }: InputProps) => {
   );
 };
 
-export default withProperties(Input, {});
+export default withProperties(Input, { TextArea, Dropdown });
--- src/section/Courses/components/AddCourseForm.tsx
+++ src/section/Courses/components/AddCourseForm.tsx
@@ -3,6 +3,21 @@
 import MainForm from '@src/components/common/MainForm';
 import { Form, Input } from '@src/components/ui';
 
+interface Option {
+  label: string;
+  value: string;
+}
+
+export interface Course {
+  id: string;
+  title: string;
+  description: string;
+  instructorId: string;
+  duration: Option[];
+  price: number;
+  status: string;
+}
+
 const AddCourseForm = () => {
   return (
     <MainForm>
@@ -16,37 +31,57 @@ const AddCourseForm = () => {
             <div className="sm:col-span-3">
               <Form.FieldLabel title="Course name" />
               <div className="mt-2">
-                <Form.Field component={Input} name="name" />
+                <Form.Field component={Input} name="name" placeholder="Course name" />
               </div>
             </div>
             <div className="sm:col-span-3">
               <Form.FieldLabel title="Description" />
               <div className="mt-2">
-                <Form.Field component={Input} name="description" />
+                <Form.Field
+                  component={Input.TextArea}
+                  name="description"
+                  placeholder="Description"
+                />
               </div>
             </div>
             <div className="sm:col-span-3">
               <Form.FieldLabel title="Duration" />
               <div className="mt-2">
-                <Form.Field component={Input} name="duration" />
+                <Form.Field
+                  component={Input.Dropdown}
+                  name="duration"
+                  options={[
+                    { value: '5mins', label: '5 Minutes' },
+                    { value: '10mins', label: '10 Minutes' },
+                    { value: '15mins', label: '15 Minutes' },
+                  ]}
+                />
               </div>
             </div>
             <div className="sm:col-span-3">
-              <Form.FieldLabel title="Teacher" />
+              <Form.FieldLabel title="Instructor" />
               <div className="mt-2">
-                <Form.Field component={Input} name="teacher" />
+                <Form.Field component={Input} name="instructor" />
               </div>
             </div>
             <div className="sm:col-span-3">
               <Form.FieldLabel title="Price" />
               <div className="mt-2">
-                <Form.Field component={Input} name="price" />
+                <Form.Field type="number" component={Input} name="price" />
               </div>
             </div>
             <div className="sm:col-span-3">
-              <Form.FieldLabel title="Country" />
+              <Form.FieldLabel title="Status" />
               <div className="mt-2">
-                <Form.Field component={Input} name="title" />
+                <Form.Field
+                  component={Input.Dropdown}
+                  name="status"
+                  options={[
+                    { value: 'published', label: 'Published' },
+                    { value: 'draft', label: 'Draft' },
+                  ]}
+                  onChange={evt => console.log('PDebug Selected country:', evt)}
+                />
               </div>
             </div>
           </div>

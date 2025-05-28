bf94cbbe42d93c082b137dad50938cb0d5bfefc0
KhaLeᴮᵉᵗᵗᵉʳ ᴱᵛᵉʳʸᵈᵃʸ <minhkhanb@gmail.com>
2025-05-28 11:44:29 +0700

    chore: fix modal issue (#67)
    Co-authored-by: Kha Le <khale.fullstack@gmail.com>

--- src/app/courses/@modal/add/page.tsx
+++ src/app/courses/@modal/add/page.tsx
@@ -2,28 +2,7 @@
 
 import React from 'react';
 import AddCourseDrawer from '@src/section/Courses/components/AddCourseDrawer';
-import { useRouter } from 'next/navigation';
 
 export default function Page() {
-  const [visible, setVisible] = React.useState(true);
-  const [shouldRedirect, setShouldRedirect] = React.useState(false);
-  const router = useRouter();
-
-  React.useEffect(() => {
-    if (!visible && shouldRedirect) {
-      const timeout = setTimeout(() => {
-        router.refresh();
-        router.push('/courses');
-      }, 300);
-
-      return () => clearTimeout(timeout);
-    }
-  }, [visible, shouldRedirect, router]);
-
-  const onClose = () => {
-    setVisible(false);
-    setShouldRedirect(true);
-  };
-
-  return <AddCourseDrawer visible={visible} onClose={onClose} />;
+  return <AddCourseDrawer visible />;
 }

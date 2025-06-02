e69f7e9ad04ad2750838a1a2315a8b4b9114f741
Vu Tien Duc <Vutienduc26122002@gmail.com>
2025-06-02 13:38:44 +0700

    chore: destructuring projects

---
+++ src/core/constants/pagination.ts
@@ -0,0 +1,10 @@
+import { Pagination } from "@/queries/useTasksQuery";
+
+export const defaultPagination: Pagination = {
+  page: 0,
+  limit: 0,
+  total: 0,
+  hasNextPage: false,
+  hasPreviousPage: false,
+  totalPages: 0,
+};

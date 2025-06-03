0e0107fd5c5b4f2c3fabebbd2d5b3aef1cafffc5
Vu Tien Duc <Vutienduc26122002@gmail.com>
2025-06-03 11:23:31 +0700

    refactor: restructuring layout and providers

---
+++ src/components/LayoutWrapper/index.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { usePathname } from "next/navigation";
+import React from "react";
+import Layout from "../Layout";
+import AuthProvider from "../providers/AuthProvider";
+
+const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+
+  const ignoreLayoutRoutes = ["/", "/sign-up"];
+
+  if (ignoreLayoutRoutes.includes(pathname))
+    return <AuthProvider>{children}</AuthProvider>;
+
+  return (
+    <AuthProvider>
+      <Layout>{children}</Layout>
+    </AuthProvider>
+  );
+};
+
+export default LayoutWrapper;
--- src/components/Providers/TaskProvider.tsx
+++ src/components/Providers/TaskProvider.tsx
@@ -1,108 +1,39 @@
-'use client';
-import { addTask, deleteTask, fetchTasks, Pagination, updateTask } from '@src/services/taskService';
-import React, { createContext, useCallback, useContext, useEffect, useState } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
+import { create } from "zustand";
+import { combine } from "zustand/middleware";
+
+export type TaskLevel = "hard" | "medium" | "easy";
+
+export type TaskFilter = "all-tasks" | "completed-tasks";
 
 export interface Task {
   id: string;
   title: string;
+  level: TaskLevel;
   subtitle: string;
   isComplete: boolean;
-  slug: string;
 }
 
-export interface TaskContextTypes {
-  pagination: Pagination | undefined;
-  tasks: Task[];
-  isLoading: boolean;
-  currentPage: number;
-  numberOfCompleted: number;
-  currentFilter: TaskFilter;
-  setCurrentFilter: React.Dispatch<React.SetStateAction<TaskFilter>>;
-  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
-  handleAddTask: ({ title, subtitle }: { title: string; subtitle: string }) => void;
-  handleUpdateTask: (task: Task) => void;
-  handleRemoveTask: (id: string) => void;
+interface TaskState {
+  limit: number;
+  page: number;
+  filter: TaskFilter;
 }
 
-export type TaskFilter = 'all-tasks' | 'completed-tasks';
+interface TaskActions {
+  setLimit: (newLimit: TaskState["limit"]) => void;
+  setPage: (newPage: TaskState["page"]) => void;
+  setFilter: (newFilter: TaskState["filter"]) => void;
+}
 
-const TaskContext = createContext<TaskContextTypes | null>(null);
-
-const TaskProvider = ({ children }: { children: React.ReactNode }) => {
-  const [pagination, setPagination] = useState<Pagination>();
-
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const [isLoading, setIsLoading] = useState(false);
-
-  const [tasks, setTasks] = useState<Task[]>([]);
-
-  const [numberOfCompleted, setNumberOfCompleted] = useState<number>(0);
-
-  const [currentFilter, setCurrentFilter] = useState<TaskFilter>('all-tasks');
-
-  const getPaginationTask = useCallback(async () => {
-    const paginatedTasks = await fetchTasks(currentPage, currentFilter);
-    if (!paginatedTasks) return;
-    setTasks(paginatedTasks.data.tasks);
-    setPagination(paginatedTasks.pagination);
-    setCurrentPage(paginatedTasks.pagination.page);
-    setNumberOfCompleted(paginatedTasks.data.numberOfCompleted);
-  }, [currentPage, currentFilter]);
-
-  useEffect(() => {
-    const loadTasks = async () => {
-      setIsLoading(true);
-      await getPaginationTask();
-      setIsLoading(false);
-    };
-    loadTasks();
-  }, [currentPage, currentFilter]);
-
-  const handleAddTask = async (task: { title: string; subtitle: string }) => {
-    await addTask(task);
-    await getPaginationTask();
-  };
-
-  const handleUpdateTask = async (task: Task) => {
-    await updateTask(task);
-    await getPaginationTask();
-  };
-
-  const handleRemoveTask = async (id: string) => {
-    await deleteTask(id);
-    await getPaginationTask();
-  };
-
-  return (
-    <TaskContext.Provider
-      value={{
-        currentFilter,
-        numberOfCompleted,
-        pagination,
-        tasks,
-        isLoading,
-        currentPage,
-        setCurrentFilter,
-        setCurrentPage,
-        handleAddTask,
-        handleUpdateTask,
-        handleRemoveTask,
-      }}
-    >
-      {children}
-    </TaskContext.Provider>
-  );
-};
-
-export const useTask = () => {
-  const context = useContext(TaskContext);
-
-  if (!context) {
-    throw new Error('useTask must be used within a TaskProvider');
-  }
-
-  return context;
-};
-
-export default TaskProvider;
+export const useTaskStore = create(
+  combine<TaskState, TaskActions>(
+    { limit: 6, page: 1, filter: "all-tasks" },
+    (set, get) => ({
+      setLimit: (newLimit: number) => set(() => ({ limit: newLimit })),
+      setPage: (newPage: number) => set(() => ({ page: newPage })),
+      setFilter: (newFilter: TaskFilter) => set(() => ({ filter: newFilter })),
+    })
+  )
+);

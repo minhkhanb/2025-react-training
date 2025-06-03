c0f2de6b2588b1fab17460ac0e9f89f7ff49c989
Bao Duong <dhbao2505@gmail.com>
2025-06-03 16:07:03 +0700

    refactor: change Todo ID type from number to string

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -4,13 +4,13 @@ import { Todo, TodoFormValues, PaginatedResponse } from '@src/types/todo';
 export const getTodos = (params: { page: number; limit: number }) =>
   get<PaginatedResponse<Todo>>('/todos', { queryParams: params });
 
-export const getTodoById = (id: number) => get<Todo>('/todos/:id', { pathParams: { id } });
+export const getTodoById = (id: string) => get<Todo>('/todos/:id', { pathParams: { id } });
 
 export const createTodo = (data: TodoFormValues) => post<Todo, TodoFormValues>('/todos', data);
 
-export const updateTodo = (id: number, data: Partial<TodoFormValues>) =>
+export const updateTodo = (id: string, data: Partial<TodoFormValues>) =>
   put<Todo, Partial<TodoFormValues>>('/todos/:id', data, {
     pathParams: { id },
   });
 
-export const deleteTodo = (id: number) => del<null>('/todos/:id', { pathParams: { id } });
+export const deleteTodo = (id: string) => del<null>('/todos/:id', { pathParams: { id } });

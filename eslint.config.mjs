67990c82489370c266e54b4fec8cc4db4d3e11ee
Khanh Nguyen <nguyenquockhanhkz20@gmail.com>
2025-06-04 09:29:26 +0700

    refactor setup environment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,38 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
+  baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-    ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
-    {
-        plugins: {
-            prettier: prettier,
-        },
-        rules: {
-            'prettier/prettier': 'error',
-            '@typescript-eslint/no-unused-vars': [
-                'error',
-                {
-                    argsIgnorePattern: '^_',
-                    caughtErrorsIgnorePattern: '^_',
-                    varsIgnorePattern: '^_',
-                },
-            ],
-            'arrow-body-style': 'off',
-            'prefer-arrow-callback': 'off',
-            'react/display-name': 'off',
-        },
-    },
-];
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
 
-export default eslintConfig;
+  {
+    files: ['**/*.{js,ts,jsx,tsx}'],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      quotes: ['error', 'single', { avoidEscape: true }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'arrow-body-style': 'off',
+      'prefer-arrow-callback': 'off',
+      'react/display-name': 'off',
+    },
+  },
+];

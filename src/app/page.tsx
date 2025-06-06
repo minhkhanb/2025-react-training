43095fad930116d13687cfe97ec52ec7026605c2
Nguyễn Quốc Khánh <76035185+nqkcodigent@users.noreply.github.com>
2025-06-06 15:45:59 +0700

    Feat(form): add form validation using react hook form and yup (#118)
    * feat(form): add form validation using react hook form and yup
    
    * fix(form): refactor the form and write common ui form
    
    * refactor(form): restructure directory and create custom form field affter reviews

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,7 @@
-'use client';
-import ToastButton from '../components/providers/Toast/ToastButton';
+import Home from '../section/Home';
 
-export default function Home() {
-  return (
-    <div className="flex flex-col gap-4 py-12 justify-center items-center">
-      <div className="font-bold">Welcome to Toaster</div>
-      <ToastButton type="info" message="This is a toast notification info" />
-      <ToastButton type="error" message="This is a toast notification error" />
-      <ToastButton
-        type="warning"
-        message="This is a toast notification warning"
-      />
-      <ToastButton
-        type="success"
-        title="Toast title"
-        message="This is a toast notification success"
-        duration={500000}
-      />
-    </div>
-  );
-}
+const HomePage = () => {
+  return <Home />;
+};
+
+export default HomePage;
---
+++ src/components/ui/InputField/Input.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import React from 'react';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  error?: string;
+  id: string;
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ label, error, id, className, ...rest }, ref) => {
+    return (
+      <div className="flex flex-col mt-4">
+        {label && (
+          <label
+            htmlFor={id}
+            className="mb-1 text-xs font-semibold tracking-wide text-gray-600 hover:text-gray-900 cursor-pointer"
+          >
+            {label}
+          </label>
+        )}
+
+        <input
+          id={id}
+          ref={ref}
+          {...rest}
+          className={`rounded-md border text-black placeholder:text-gray-500 px-3 py-2 text-base leading-6 bg-white focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-opacity-50 ${
+            error ? 'border-red-500' : 'border-gray-500'
+          } ${className}`}
+        />
+      </div>
+    );
+  }
+);
+
+Input.displayName = 'Input';
+
+export default Input;

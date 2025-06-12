2485384f885aae9bd32d3f9a1a883331eeee85d8
Khanh Nguyen <nguyenquockhanhkz20@gmail.com>
2025-06-12 14:07:47 +0700

    refactor(todo): Update code after review and optimize performance

--- src/section/Todos/components/FormInput.tsx
+++ src/section/Todos/components/FormInput.tsx
@@ -1,13 +1,21 @@
+import { ElementType } from 'react';
+import { FieldValues, Path } from 'react-hook-form';
 import { Label } from '@/src/components/shadcn/ui/label';
 import { FormField } from '@/src/components/ui/Form';
 
-type FormInputProps = {
-  name: string;
+type FormInputProps<TFieldValues extends FieldValues> = {
+  name: Path<TFieldValues>;
   label: string;
   placeholder: string;
-  component: React.ElementType;
+  component: ElementType;
 };
-const FormInput = ({ name, label, placeholder, component }: FormInputProps) => {
+
+const FormInput = <TFieldValues extends FieldValues>({
+  name,
+  label,
+  placeholder,
+  component,
+}: FormInputProps<TFieldValues>) => {
   return (
     <div className="mb-4">
       <Label htmlFor={name as string} className="mb-1 block">
